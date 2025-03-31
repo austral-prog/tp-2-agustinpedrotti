bc81c3bebe0453d1542b7fb3d00ae8f143c7b4d1
agustinpedrotti <apedrotti1@mail.austral.edu.ar>
2025-03-31 12:06:37 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -4,4 +4,4 @@ def ada():
     print(first_name.lower() + " " + last_name.lower())
     print(first_name.capitalize() + " " + last_name.capitalize())
     print(first_name.upper() + " " + last_name.upper())
-    print(f"\t first_name.lower() +  + last_name.lower()")
+    print(f"\tada lovelace")
