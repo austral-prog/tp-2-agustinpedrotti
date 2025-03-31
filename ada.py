73e56ecfb97101e8e448735d8e656b53a065f617
agustinpedrotti <apedrotti1@mail.austral.edu.ar>
2025-03-31 11:56:46 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -2,6 +2,6 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
     print(first_name.lower() + " " + last_name.lower())
-    print(first_name.capital() + " " + last_name.lower())
+    print(first_name.capitalize() + " " + last_name.capitalize())
     print(first_name.upper() + " " + last_name.lower())
     print(f"\t first_name.lower() + " " + last_name.lower()")
